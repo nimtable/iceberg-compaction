a03235351b49a39e986c9783c6d812dc39fa0f06
Li0k <yuli@singularity-data.com>
2025-03-28 15:54:24 +0800

    feat(IC): support web server

--- services/compactor/src/bin/compactor-server.rs
+++ services/compactor/src/bin/compactor-server.rs
@@ -1,9 +1,34 @@
-use ic_service_compactor::server::compactor_serve;
+use ic_service_compactor::server::{grpc_compactor_serve, http_compactor_serve};
 
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 #[tokio::main]
 async fn main() {
-    let listen_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 7777);
-    compactor_serve(listen_addr).await;
+    // read ip and port from env
+    let listen_addr = {
+        let default_ip = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+        let default_port = 7777;
+
+        let ip_str = std::env::var("ADDR_IP").unwrap_or_else(|_| default_ip.to_string());
+        let ip: IpAddr = ip_str.parse().unwrap_or(default_ip);
+
+        let port_str = std::env::var("ADDR_PORT").unwrap_or_else(|_| default_port.to_string());
+        let port: u16 = port_str.parse().unwrap_or(default_port);
+
+        SocketAddr::new(ip, port)
+    };
+
+    let server_type = std::env::var("SERVER_TYPE").unwrap_or_else(|_| "grpc".to_string());
+
+    let _join_handle = match server_type.as_str() {
+        "grpc" => {
+            println!("Starting gRPC server...");
+            grpc_compactor_serve(listen_addr).await
+        }
+        "http" => {
+            println!("Starting HTTP server...");
+            http_compactor_serve(listen_addr).await
+        }
+        _ => panic!("Unknown server type: {}", server_type),
+    };
 }
--- services/compactor/src/rpc.rs
+++ services/compactor/src/rpc.rs
@@ -1,4 +1,3 @@
-use futures::Stream;
 use ic_prost::compactor::compactor_service_server::CompactorService;
 use ic_prost::compactor::{RewriteFilesRequest, RewriteFilesResponse};
 
@@ -7,16 +6,12 @@ pub struct CompactorServiceImpl;
 
 #[async_trait::async_trait]
 impl CompactorService for CompactorServiceImpl {
-    type RewriteFilesStream =
-        std::pin::Pin<Box<dyn Stream<Item = Result<RewriteFilesResponse, tonic::Status>> + Send>>;
-
     async fn rewrite_files(
         &self,
-        _request: tonic::Request<tonic::Streaming<RewriteFilesRequest>>,
-    ) -> std::result::Result<tonic::Response<Self::RewriteFilesStream>, tonic::Status> {
+        _request: tonic::Request<RewriteFilesRequest>,
+    ) -> std::result::Result<tonic::Response<RewriteFilesResponse>, tonic::Status> {
         //TODO: compact the input files with executor
 
-        let empty_output_stream = futures::stream::empty();
-        Ok(tonic::Response::new(Box::pin(empty_output_stream)))
+        Ok(tonic::Response::new(RewriteFilesResponse {}))
     }
 }
