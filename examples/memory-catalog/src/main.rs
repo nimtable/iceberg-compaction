4d40b932f2334f9258c6961e0a93076b5cae4932
Dylan <zilin@singularity-data.com>
2025-06-25 16:12:59 +0800

    feat: add a rest catalog example (#51)
    Co-authored-by: Li0k <yuli@singularity-data.com>

--- examples/memory-catalog/src/main.rs
+++ examples/memory-catalog/src/main.rs
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 iceberg-compaction
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 use std::collections::HashMap;
 use std::sync::Arc;
 use tempfile::TempDir;
@@ -21,7 +37,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let catalog = Arc::new(MemoryCatalog::new(file_io, Some(warehouse_location)));
 
     // 3. Create namespace and table
-    let namespace_ident = NamespaceIdent::new("warehouse".into());
+    let namespace_ident = NamespaceIdent::new("my_namespace".into());
     catalog
         .create_namespace(&namespace_ident, HashMap::new())
         .await?;
